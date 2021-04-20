cff1a20673b88b9ea41faf2e0c980eb1cf54ab8b
bettapo <bettapo@yandex-team.ru>
2021-04-20 13:45:12 +0300

    Пункты 1 и 3

---
+++ main.js
@@ -0,0 +1,55 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("./index.json", function(text){
+    window.books = JSON.parse(text);
+});
+
+/* Методы работы с книгам */
+
+function showBooks() {
+    var authorFilter = document.getElementById('author').value;
+    var inStockFilter = document.getElementById('inStock').checked;
+
+    if (!authorFilter && !inStockFilter) {
+        return books;
+    }
+
+    var res;
+
+    if (inStockFilter) {
+        res = books.filter(function(book) {
+            return book.count > 0;
+        });
+    }
+
+    if (authorFilter) {
+        res = books.filter(function(book) {
+            return book.author.indexOf(authorFilter) > -1;
+        });
+    }
+
+    return res;
+}
+
+function addBook(book) {
+    books.push(book);
+}
+
+function deleteBook(book) {
+    var index = books.findIndex(book);
+    return books.splice(index, 1);
+}
+
+
+
+
